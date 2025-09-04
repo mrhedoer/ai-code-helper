faeb6a3dd2b64780e4bec3bbab49f02e816bb3cd
mrhedoer <1031957521@qq.com>
2025-09-04 14:18:41 +0800

    1

---
+++ src/test/java/com/hejunhao/aicodehelper/AiCodeHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hejunhao.aicodehelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCodeHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
