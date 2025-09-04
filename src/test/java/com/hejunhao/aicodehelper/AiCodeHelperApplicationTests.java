dc699b08a5238525ffa5baf7ea29762d89be65cc
mrhedoer <1031957521@qq.com>
2025-09-04 15:14:26 +0800

    1

--- src/test/java/com/hejunhao/aicodehelper/AiCodeHelperApplicationTests.java
+++ src/test/java/com/hejunhao/aicodehelper/AiCodeHelperApplicationTests.java
@@ -1,13 +1,16 @@
 package com.hejunhao.aicodehelper;
 
+import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AiCodeHelperApplicationTests {
-
+    @Resource
+    private AiCodeHelper aiCodeHelper;
     @Test
     void contextLoads() {
+        aiCodeHelper.chat("你好，我是程序员6");
     }
 
 }
