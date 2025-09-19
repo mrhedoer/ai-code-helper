aa3deb0ec90a093256056b1acc3ef076394b8874
mrhedoer <1031957521@qq.com>
2025-09-19 20:26:41 +0800

    1

--- src/main/java/com/hejunhao/aicodehelper/model/QwenChatModelConfig.java
+++ src/main/java/com/hejunhao/aicodehelper/model/QwenChatModelConfig.java
@@ -5,17 +5,18 @@ import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.model.chat.listener.ChatModelListener;
 import jakarta.annotation.Resource;
 import lombok.Data;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
 @Configuration
-@ConfigurationProperties(prefix = "langchain4j.community.dashscope.chat-model")
 @Data
 public class QwenChatModelConfig {
+    @Value("${langchain4j.community.dashscope.chat-model.model-name}")
     private String modelName;
+    @Value("${langchain4j.community.dashscope.chat-model.api-key}")
     private String apiKey;
     @Resource
     private ChatModelListener chatModelListener;
